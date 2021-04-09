11e8ffbf9c0fc1f931a221264893ab66f3252dc2
dcheever <cheeverdanny@gmail.com>
2021-04-08 19:58:07 -0500

    add api 2

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 fetch("https://www.scorebat.com/video-api/v1/")
     .then(response => response.json())
     .then(json => console.log(json));
+
+ fetch("https://api.the-odds-api.com/v3/odds/?apiKey=8509b882b891fad9105002e5e52276cc&sport=soccer_epl&region=uk&mkt=h2h")
+        .then(response => response.json())
+        .then(json => console.log(json));
