5c8d10e6ee28ed33c535e4e402591e5ead5d7ee0
Vasilyg10 <zvgianna@gmail.com>
2021-04-11 22:29:27 -0500

    worked on youtube api

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // Fetch Team Highlights
-fetch("https://www.scorebat.com/video-api/v1/")
-    .then(response => response.json())
-    .then(json => console.log(json));
+fetch("https://www.googleapis.com/youtube/v3/search?key=AIzaSyAFFU4jeWp4CulQBzEPnVz-mnODODs_Rpg&type=video&q=liverpool")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        let videoEl = document.getElementById("video")
+
+        response.setAttribute("src", "https://www.youtube.com/embed/9PAIC8Yv29s?autoplay=1")
+
+
+        videoEl.append(response);
+    });
 
 // Fetch Match Odds
- fetch("https://api.the-odds-api.com/v3/odds/?apiKey=8509b882b891fad9105002e5e52276cc&sport=soccer_epl&region=uk&mkt=h2h")
-        .then(response => response.json())
-        .then(json => console.log(json));
+//  fetch("https://api.the-odds-api.com/v3/odds/?apiKey=8509b882b891fad9105002e5e52276cc&sport=soccer_epl&region=uk&mkt=h2h")
+//         .then(response => response.json())
+//         .then(json => console.log(json));
 
 
 // This API for team crest / league table / previous fixtures if we need/want to add
 // fetch("https://www.thesportsdb.com/api/v1/json/1/eventsround.php?id=4328&r=31&s=2020-2021&api_key=1")
 //         .then(response => response.json())
 //         .then(json => console.log(json));
-
-// The below code is the JavaScript supplied by the scorebat.com embed code
-
-// <script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = 'https://www.scorebat.com/embed/embed.js?v=arrv'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'scorebat-jssdk'));</script>
